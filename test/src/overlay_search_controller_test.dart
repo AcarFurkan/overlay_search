c4c898560b951c114340f114f53f9e41532fa4da
furkan acar <65075121+AcarFurkan@users.noreply.github.com>
2024-02-27 10:35:31 +0300

    adding right test structure

---
+++ test/src/overlay_search_controller_test.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:overlay_search/overlay_search.dart';
+
+void main() {
+  group('OverlaySearchController Tests', () {
+    late OverlaySearchController controller;
+
+    setUp(() {
+      controller = OverlaySearchController();
+    });
+
+    test('clearSearchQuery should clear searchController text', () {
+      controller.searchController.text = 'test';
+      controller.clearSearchQuery();
+      expect(controller.searchController.text, isEmpty);
+    });
+
+    test('updateStocks should filter and update itemList based on searchKey',
+        () {
+      final List<OverlayItemModel> mockItems = [
+        OverlayItemModel(title: 'Apple'),
+        OverlayItemModel(title: 'Banana'),
+        OverlayItemModel(title: 'Cherry'),
+      ];
+      controller.updateStocks(mockItems, searchKey: 'a');
+      expect(controller.itemList.length, 2);
+      expect(controller.itemList[0].title, 'Apple');
+      expect(controller.itemList[1].title, 'Banana');
+    });
+
+    test('updateLoading should update isLoading state', () {
+      expect(controller.isLoading, isFalse);
+      controller.updateLoading(true);
+      expect(controller.isLoading, isTrue);
+    });
+  });
+}
